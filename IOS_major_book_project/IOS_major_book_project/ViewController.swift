870f872fb649d6501d5324ed3c36d985b834283c
Kimminjung <poly9010@naver.com>
2023-02-03 19:36:43 +0900

    프로젝트 파일 생성

---
+++ IOS_major_book_project/IOS_major_book_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS_major_book_project
+//
+//  Created by 김민정 on 2023/02/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
